e17baae448e7b87154490c6abcb74662d820d5f3
dmeowmixer <ransonikuo@gmail.com>
2019-10-03 23:14:16 -1000

    possibly completed setting vars for fields

--- README.md
+++ README.md
@@ -1,19 +1,8 @@
 # INSTRUCTIONS
 * Running localhost server on port:8000 w/ Python -m 'SimpleHTTPServer'
-* Navigate to http://localhost:8000/ and drag Word Count Visualizer link to Bookmarks bar
-* Navigate to https://themeisle.com/blog/best-free-blogging-sites/ and click Word Count Visualizer
-
-# Assumptions:
-* Website analyzed - https://themeisle.com/blog/best-free-blogging-sites/
-* Words are separated by Spaces (currently targeting p tags), however, additional tags can be targeted and added to the wordArray before processing.
-* Browser - Chrome 71, JS Enabled
 
 # Notes:
-* <div id="sitePenResultsDiv"> created on running bookmarklet code, will append to bottom of page
-* Sizes are set utilizing creation of h1 to h4 tags (I tried setting a fontSize property which I could not get to work despite trying to clear styles. Would try to reimplement variable appeared * 10 fontSize given more time for better representation of # of word appearances)
-* Did not minify code for readability purposes
+* Did not minify code for readability/adjustability
 
 # ToDo: 
-* Dynamically set font-size attribute
-* Append to top of page
-* Adjust placement of words instead of list
+
--- app.js
+++ app.js
@@ -2,58 +2,72 @@
 (function(){
   // Verification <script> element has been loaded into DOM.
   console.log('Ready');
-  var wordArray = [];
-  var wordObj = {};
-  var sortedObj = {};
-// create DIV element to target and push wordCloud
-  let div = document.createElement('div');
-  div.id = 'sitePenResultsDiv';
+  let emailField = document.getElementById('checkout_email');
+  let firstNameField = document.getElementById('checkout_shipping_address_first_name');
+  let lastNameField = document.getElementById('checkout_shipping_address_last_name');
+  let addressField = document.getElementById('checkout_shipping_address_address1');
+  let cityField = document.getElementById('checkout_shipping_address_city');
+  let countrySelect = document.getElementById('checkout_shipping_address_country');
+  let stateSelect = document.getElementById('checkout_shipping_address_province');
+  let zipcodeField = document.getElementById('checkout_shipping_address_zip');
+  let phoneField = document.getElementById('checkout_shipping_address_phone');
+  emailField.value('asdf');
+  firstNameField.value('asdf');
+  lastNameField.value('fdsa');
 
-  div.style.cssText = 'position: absolute; width: 100%; height: 100%;'
-  document.body.appendChild(div);
 
-  // Currently targets all <p>
-  document.querySelectorAll('p').forEach(x => {
-    wordArray.push(x.innerText.split(" "));
-  });
+//   var wordArray = [];
+//   var wordObj = {};
+//   var sortedObj = {};
+// // create DIV element to target and push wordCloud
+//   let div = document.createElement('div');
+//   div.id = 'sitePenResultsDiv';
 
-  // Should ideally handle key alphabetize sorting in wordArray.forEach (refactoring)
-  wordArray.forEach(x => {
-    for(var i = 0; i < x.length; i++){
-      var num = x[i];
-      // Check against word length
-      if(x[i].length >= 4){
-        // If current iteration exists, increate conut value, if not instantiate key and count to 1
-        wordObj[num] = wordObj[num] ? wordObj[num] +1 : 1;
-      }
-    }
-  })
-  Object.keys(wordObj).sort().forEach(function(key){
-    sortedObj[key] = wordObj[key];
-  })
+//   div.style.cssText = 'position: absolute; width: 100%; height: 100%;'
+//   document.body.appendChild(div);
 
-  // TODO
-  // ensure word length check is working properly
-  // refactor
+//   // Currently targets all <p>
+//   document.querySelectorAll('p').forEach(x => {
+//     wordArray.push(x.innerText.split(" "));
+//   });
 
-  for (var i in sortedObj){
-    if(sortedObj.hasOwnProperty(i)){
-      let appeared = sortedObj[i];
-      var createdEle;
-      if (appeared >= 6){
-        createdEle = document.createElement('h1');
-      } else if (appeared >= 4){
-        createdEle = document.createElement('h2');
-      } else if (appeared >= 2 ){
-        createdEle = document.createElement('h3');
-      } else{
-        createdEle = document.createElement('h4');
-      }
+//   // Should ideally handle key alphabetize sorting in wordArray.forEach (refactoring)
+//   wordArray.forEach(x => {
+//     for(var i = 0; i < x.length; i++){
+//       var num = x[i];
+//       // Check against word length
+//       if(x[i].length >= 4){
+//         // If current iteration exists, increate conut value, if not instantiate key and count to 1
+//         wordObj[num] = wordObj[num] ? wordObj[num] +1 : 1;
+//       }
+//     }
+//   })
+//   Object.keys(wordObj).sort().forEach(function(key){
+//     sortedObj[key] = wordObj[key];
+//   })
 
-      createdEle.innerHTML = i;
-      createdEle.style.cssText = 'width: 100%;color: black; margin: 10px; font-size: "";';
+//   // TODO
+//   // ensure word length check is working properly
+//   // refactor
+
+//   for (var i in sortedObj){
+//     if(sortedObj.hasOwnProperty(i)){
+//       let appeared = sortedObj[i];
+//       var createdEle;
+//       if (appeared >= 6){
+//         createdEle = document.createElement('h1');
+//       } else if (appeared >= 4){
+//         createdEle = document.createElement('h2');
+//       } else if (appeared >= 2 ){
+//         createdEle = document.createElement('h3');
+//       } else{
+//         createdEle = document.createElement('h4');
+//       }
+
+//       createdEle.innerHTML = i;
+//       createdEle.style.cssText = 'width: 100%;color: black; margin: 10px; font-size: "";';
       
-      div.appendChild(createdEle);
-    }
-  }
+//       div.appendChild(createdEle);
+//     }
+//   }
 }())
